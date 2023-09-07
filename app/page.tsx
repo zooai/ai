93980f01211beaa91ff698e752614d6d268559c9
woobin <woobinpro@gmail.com>
2023-09-07 06:11:56 -0700

    woo0907

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
-
+import { Navbar }  from '@/components/navbar'
 export const runtime = 'edge'
 
 export default function IndexPage() {
   const id = nanoid()
 
-  return <Chat id={id} />
+  return <div className='flex flex-col'>
+      {/* @ts-ignore */}
+      <Navbar />
+      <Chat id={id} />
+    </div>
 }
---
+++ components/slider.tsx
@@ -0,0 +1,35 @@
+import Slider from 'react-slick';
+import Image from 'next/image';
+function Sideslider(props:any) {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        autoplay: true,
+        arrows: false
+      };
+    return (
+        <div className='w-full h-[180px] opacity-50'>
+        <Slider {...settings} className='bg-black'>
+            <Image
+                className='w-full h-[180px]'
+                src='/bg.webp'
+                width='300'
+                height='300'
+                alt=''
+            />
+            <Image
+                className='w-full h-[180px]'
+                src='/slider2.jfif'
+                width='300'
+                height='300'
+                alt=''
+            />
+        </Slider>
+        </div>
+    )
+}
+export default Sideslider
