25cb271516d734930af23cfae784ae55c48f8c87
woobin <woobinpro@gmail.com>
2023-10-12 09:28:14 -0700

    fixed the github issues

--- components/home/investment.tsx
+++ components/home/investment.tsx
@@ -5,64 +5,56 @@ import { styled } from '@mui/material/styles';
 import money_icon from '../../public/money_icon.png'
 import { FaAngleRight } from 'react-icons/fa'
 import Image from 'next/image'
+import { Range } from 'react-range';
 function Investment() {
-  const [hour ,setHour] = useState(20);
+  const [hour ,setHour] = useState({ values: [20] });
   const [rate ,setRate] = useState(50);
-  const CustomSlider = styled(Slider)({
-      color: '#2929EB',
-      height: 8,
-      '& .MuiSlider-track': {
-        border: 'none',
-      },
-      '& .MuiSlider-thumb': {
-        height: 15,
-        width: 15,
-        backgroundColor: '#fff',
-        borderRadius: 2,
-        border: '2px solid #fff',
-        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-          boxShadow: 'inherit',
-        },
-        '&:before': {
-          display: 'none',
-        },
-      },
-      '& .MuiSlider-valueLabel': {
-        lineHeight: 1.2,
-        fontSize: 12,
-        background: 'unset',
-        padding: 0,
-        width: 32,
-        height: 32,
-        borderRadius: '50% 50% 50% 0',
-        backgroundColor: '#333',
-        transformOrigin: 'bottom left',
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-        '&:before': { display: 'none' },
-        '&.MuiSlider-valueLabelOpen': {
-          transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-        },
-        '& > *': {
-          transform: 'rotate(45deg)',
-        },
-      },
-    });
-  const handleHourChange = (event: Event, newValue: number | number[]) => {
-      if (typeof newValue === 'number') {
-        setHour(newValue);
-      }
-    };
-  const handleInputChange = (event: any) => {
+  
+   const handleInputChange = (event: any) => {
       setRate(event.target.value);
     };
     return (
-      <div className='bg-black py-32 px-64 max-md:py-24 max-md:px-8'>
+      <div className='bg-black py-32 px-64 max-md:py-12 max-md:px-8'>
         <div className="flex max-md:flex-col items-center justify-between">
             <div className="w-2/5 max-md:w-full flex flex-col gap-4 pr-16 max-md:px-16 max-sm:px-8">
                 <p className='text-xs text-[#8d8d8e]'>Hours lost per week on distractions, context-switching, manual writing work, and automating repetitive tasks.</p>
-                <p className='text-lg'>{hour} Hours / week</p>
-                <CustomSlider valueLabelDisplay="auto" aria-label="sense slider" value={hour} step={1} onChange={handleHourChange} min={1} max={20} />
-                <div className="px-1 flex justify-between -mt-6">
+                <p className='text-lg'>{hour.values[0]} Hours / week</p>
+                {/* <CustomSlider valueLabelDisplay="auto" aria-label="sense slider" value={hour} step={1} onChange={handleHourChange} min={1} max={20} /> */}
+                <Range
+                  step={1}
+                  min={1}
+                  max={20}
+                  values={hour.values}
+                  onChange={(values) => setHour({values})}
+                  renderTrack={({ props, children }) => (
+                    <div
+                      {...props}
+                      style={{
+                        ...props.style,
+                        height: '8px',
+                        width: '100%',
+                        backgroundColor: '#2929EB',
+                        borderRadius:4
+                      }}
+                    >
+                      {children}
+                    </div>
+                  )}
+                  renderThumb={({ props }) => (
+                    <div
+                      {...props}
+                      style={{
+                        ...props.style,
+                        height: '15px',
+                        width: '15px',
+                        backgroundColor: '#fff',
+                        borderRadius: 2,
+                        border: '2px solid #fff',
+                      }}
+                    />
+                  )}
+                />
+                <div className="px-1 flex justify-between -mt-3">
                     <p className="text-[0.5rem]">1</p>
                     <p className="text-[0.5rem]">20</p>
                 </div>
@@ -77,7 +69,7 @@ function Investment() {
                 <div className='text-3xl max-md:text-center'><span className='text-[#8787f7]'>Return</span> on investment</div>
                 <div className='text-sm'>Per month with ZOO AI</div>
                 <div className='text-5xl pt-5 flex items-center justify-center'>
-                  <span>$</span>
+                  <span>${hour.values[0] * 4 * rate - 49}</span>
                   <Image
                         className='h-[50px]'
                         src={money_icon}
@@ -86,17 +78,17 @@ function Investment() {
                         alt=''
                     />
                 </div>
-                <div className='text-sm'>Or {hour * 4} hours of your life</div>
+                <div className='text-sm'>Or {hour.values[0] * 4} hours of your life</div>
                 <div className='w-full mt-5 bg-[#8787f7] text-white py-2 rounded-lg flex items-center justify-center gap-4'>Get Started Now <FaAngleRight /></div>
                 <hr className='w-full my-5'/>
                 <div className='w-full text-sm'>How did we get this number?</div>
                 <div className='flex justify-between items-center w-full pt-2'>
                   <span className='text-[#9a9a9b] text-xs'>Hours lost per month</span>
-                  <span className='text-white text-xs'>{hour * 4} hours</span>
+                  <span className='text-white text-xs'>{hour.values[0] * 4} hours</span>
                 </div>
                 <div className='flex justify-between items-center w-full pt-2'>
-                  <span className='text-[#9a9a9b] text-xs'>Value of ZOO AI for saving {hour * 4} hours of your time </span>
-                  <span className='text-white text-xs'>${hour * 4 * rate}</span>
+                  <span className='text-[#9a9a9b] text-xs'>Value of ZOO AI for saving {hour.values[0] * 4} hours of your time </span>
+                  <span className='text-white text-xs'>${hour.values[0] * 4 * rate}</span>
                 </div>
                 <div className='flex justify-between items-center w-full pt-2'>
                   <span className='text-[#9a9a9b] text-xs'>Cost of ZOO AI PRO subscription per month</span>
@@ -105,7 +97,7 @@ function Investment() {
                 <hr className='w-full my-5'/>
                 <div className='flex justify-between items-center w-full'>
                   <span className='text-white text-sm'>Total TOI per month</span>
-                  <span className='text-white text-xs'>${hour * 4 * rate - 49}</span>
+                  <span className='text-white text-xs'>${hour.values[0] * 4 * rate - 49}</span>
                 </div>
             </div>
         </div>
