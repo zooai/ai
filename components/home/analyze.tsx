17567ccb6e378c405da256d8f9eafd5f626af97e
woobin <woobinpro@gmail.com>
2023-10-12 02:15:17 -0700

    fixed the vercel issues

--- components/home/analyze.tsx
+++ components/home/analyze.tsx
@@ -5,7 +5,7 @@ function Analyze() {
     return (
       <div className='bg-black py-32 max-md:py-24'>
         <p className='text-5xl max-md:text-3xl text-center'>Analyze your files. Word by word.<br />Cell by cell.</p>
-        <div className='flex max-md:flex-col max-md:flex-col-reverse px-36 max-md:px-8 pt-32 max-md:pt-16 items-center justify-between'>
+        <div className='flex max-md:flex-col-reverse px-36 max-md:px-8 pt-32 max-md:pt-16 items-center justify-between'>
             <div className='space-y-4 w-1/2 mr-16 max-md:w-5/6 max-md:mr-0 max-md:pt-8'>
                 <p className='text-3xl max-md:text-2xl max-md:text-center'>Ask your buddy to monitor and set smart home controls.</p>
                 <p className='text-xs max-md:text-center'>Easily monitor and control your energy consumption when you delegate the tasks to it or just say “set up my smarthome.”</p>
