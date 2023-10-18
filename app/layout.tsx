bcef45fd08b7027392777b2a210fca30469a794f
woobin <woobinpro@gmail.com>
2023-10-18 06:00:29 -0700

    fixed the main page issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,9 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: dark)', color: 'black' }
   ],
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
+    icon: '/zooLogoLight.svg',
+    shortcut: '/zooLogoLight.svg',
+    apple: '/zooLogoLight.svg'
   }
 }
 
