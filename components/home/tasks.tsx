6ae5000271bb98ae43db8a86e9a4181c9dd53cc3
woobin <woobinpro@gmail.com>
2023-10-23 08:08:16 -0700

    fixed the gradient issue

--- components/home/tasks.tsx
+++ components/home/tasks.tsx
@@ -11,7 +11,7 @@ function Tasks() {
       <div className='bg-black p-32 max-md:py-24 max-md:px-0'>
         <p className='text-5xl font-semibold max-md:text-3xl text-center'>Avoid the fatigue from <span className='text-[#8787F7]'>repetitive tasks</span><br />when you streamline with Zoo Ai.</p>
         <div className='flex px-8 pt-32 max-md:pt-16 max-md:flex-col-reverse items-start justify-between gap-32'>
-            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-[15%] to-[#000000]'>
+            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-15% to-[#000000]'>
                 <div className='w-[1px] h-[150px] bg-[#929292]'></div>
                 <div className='flex flex-col py-8 space-y-4'>
                     <p className='text-3xl px-16 max-sm:px-4 font-semibold text-center'>A <span className='text-[#8787F7]'>friendly AI</span> for all</p>
@@ -46,7 +46,7 @@ function Tasks() {
                     <div className='button-gradient px-4 py-2 rounded-md flex items-center space-x-3'><span>Automate your life</span><FaChevronRight /></div>
                 </div>
             </div>
-            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-[15%] to-[#000000]'>
+            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-15% to-[#000000]'>
                 <div className='w-[1px] h-[150px] bg-[#929292]'></div>
                 <div className='flex flex-col py-8 space-y-4'>
                     <p className='text-3xl px-16 max-sm:px-4 font-semibold text-center'>AI that keeps working in the background</p>
@@ -56,7 +56,7 @@ function Tasks() {
             </div>
         </div>
         <div className='flex px-8 pt-32 max-md:pt-16 max-md:flex-col-reverse items-start justify-between gap-32'>
-            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-[15%] to-[#000000]'>
+            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-15% to-[#000000]'>
                 <div className='w-[1px] h-[150px] bg-[#929292]'></div>
                 <div className='flex flex-col py-8 space-y-4'>
                     <p className='text-3xl px-16 max-sm:px-4 font-semibold text-center'>All access <span className='text-[#8787F7]'>all the time</span></p>
@@ -98,7 +98,7 @@ function Tasks() {
                     <div className='button-gradient px-4 py-2 rounded-md flex items-center space-x-3'><span>Try Document Editor</span><FaChevronRight /></div>
                 </div>
             </div>
-            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-[15%] to-[#000000]'>
+            <div className='w-1/2 max-md:w-full ml-16 max-md:ml-0 max-md:z-10 justify-between flex flex-col items-center max-md:bg-gradient-to-b from-[#00000000] via-[#000000df] via-15% to-[#000000]'>
                 <div className='w-[1px] h-[150px] bg-[#929292]'></div>
                 <div className='flex flex-col py-8 space-y-4'>
                     <p className='text-3xl px-16 max-sm:px-4 font-semibold text-center'>Your virtual assistant</p>
