a8514cbe45183c55d2c53c95f17d3a6ee8293642
woobin <woobinpro@gmail.com>
2023-10-30 22:55:03 -0700

    added the stripe function

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 import { Navbar }  from '@/components/navbar'
+import * as React from 'react';
+// import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
 export const runtime = 'edge'
 
 export default function IndexPage() {
   const id = nanoid()
-
+  // const router = useRouter();
+  // React.useEffect(() => {
+  //   if (router.isReady) {
+  //     const result = router.query.result;
+  //     if(result=='success'){
+  //       toast.success("Thank you for donating!");
+  //     }else if(result=='cancelled') {
+  //       toast.warning("Donation is cancelled!!!");
+  //     }
+  //   }
+  // }, [router]);
   return <div className='flex flex-col'>
       {/* @ts-ignore */}
       <Navbar />
+      <ToastContainer />
       <Chat id={id} />
     </div>
 }
